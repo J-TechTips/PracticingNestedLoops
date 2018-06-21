b4e4f103ab2c7b97869a9b2f796304bc67ac0b8f
Jonathan <jtecktips@gmail.com>
2018-06-21 15:14:57 -0700

    Finished Practice 01

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  bools: Array<boolean> = [true, false];
+  nums: Array<number> = [1, 2.5, 5];
+  strs: Array<string> = ['hi', 'there'];
+  test: object = {
+    id: 2,
+    name: 'test'
+  };
+  user: object = {
+    id: 2,
+    name: 'user'
+  };
+  objs: Array<object> = [this.test, this.user];
+  types: Array<any> = [this.bools, this.nums, this.strs, this.objs];
+  ngOnInit() {
+
+  }
+  getTypeof(types) {
+    return typeof types;
+  }
 }
